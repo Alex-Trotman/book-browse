f280277f6604c34b38d0bee05e0f7036d417d989
Alex Trotman <alextrotman04@gmail.com>
2023-09-20 14:13:04 -0500

    Prevent the default form submission behavior

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const form = document.getElementById('form-element');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("Submit button clicked")
+    console.log("Submit button clicked");
+});
 
-    const searchQuery = document.getElementById('searchQuery').value;
-
-    let plusQuery = searchQuery.split(' ').join('+')
-
-    console.log(plusQuery)
-})
