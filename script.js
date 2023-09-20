83288536a89b70bb2941007a55fc56d561831983
Alex Trotman <alextrotman04@gmail.com>
2023-09-20 14:24:04 -0500

    Fetch data from the Open Library API and process the response

--- script.js
+++ script.js
@@ -9,5 +9,34 @@ form.addEventListener('submit', (event) => {
     console.log("plusQuery:", plusQuery);
 
     const apiUrl = `https://openlibrary.org/search.json?q=${plusQuery}`;
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+
+        const resultDiv = document.getElementById('resultDiv');
+        const ul = document.createElement('ul');
+
+        data.docs.forEach((item) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+                    
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = item.title;
+                    
+            const authorElement = document.createElement('p');
+            authorElement.textContent = `Author: ${item.author_name}`;
+                    
+            card.appendChild(titleElement);
+            card.appendChild(authorElement);
+                    
+            resultDiv.appendChild(card);
+
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
 
