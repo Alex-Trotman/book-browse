409c085416d9524108e761e7a1a1c458da3b0e32
Alex Trotman <alextrotman04@gmail.com>
2023-09-20 14:21:45 -0500

    Extract and format the user's search query for the API request

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const form = document.getElementById('form-element');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log("Submit button clicked");
+
+    const searchQuery = document.getElementById('searchQuery').value;
+    const plusQuery = searchQuery.split(' ').join('+');
+    console.log("plusQuery:", plusQuery);
 });
 
