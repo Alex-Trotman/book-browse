6140021813199f9fd156a9be4aaef11748033ea8
Alex <alextrotman04@gmail.com>
2023-09-20 19:08:52 -0500

    formats with prettier, moved form to nav element

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-const form = document.getElementById('form-element');
+const form = document.getElementById("form-element");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log("Submit button clicked");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Submit button clicked");
 
-    const searchQuery = document.getElementById('searchQuery').value;
-    const plusQuery = searchQuery.split(' ').join('+');
-    console.log("plusQuery:", plusQuery);
+  const searchQuery = document.getElementById("searchQuery").value;
+  const plusQuery = searchQuery.split(" ").join("+");
+  console.log("plusQuery:", plusQuery);
 
-    const apiUrl = `https://openlibrary.org/search.json?q=${plusQuery}`;
+  const apiUrl = `https://openlibrary.org/search.json?q=${plusQuery}`;
 
-    fetch(apiUrl)
+  fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+      console.log(data);
 
-        const resultDiv = document.getElementById('resultDiv');
-        resultDiv.innerHTML = '';
+      const resultDiv = document.getElementById("resultDiv");
+      resultDiv.innerHTML = "";
 
-        data.docs.forEach((item) => {
-            const card = document.createElement('div');
-            card.classList.add('card');
+      data.docs.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-            const titleElement = document.createElement('h2');
-            titleElement.textContent = item.title;
+        const titleElement = document.createElement("h2");
+        titleElement.textContent = item.title;
 
-            const authorElement = document.createElement('p');
-            authorElement.textContent = `Author: ${item.author_name}`;
+        const authorElement = document.createElement("p");
+        authorElement.textContent = `Author: ${item.author_name}`;
 
-            const imageElement = document.createElement('img');
-            imageElement.src = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`
+        const imageElement = document.createElement("img");
+        imageElement.src = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`;
 
-            card.appendChild(titleElement);
-            card.appendChild(authorElement);
-            card.appendChild(imageElement);
+        card.appendChild(titleElement);
+        card.appendChild(authorElement);
+        card.appendChild(imageElement);
 
-            resultDiv.appendChild(card);
-
-        });
+        resultDiv.appendChild(card);
+      });
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 });
-
